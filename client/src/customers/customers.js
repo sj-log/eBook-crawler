2a36cfb188394097b8c7f780a8da252645021a8a
SJ <sjisyours@gmail.com>
2020-01-01 11:53:59 +0900

    update

---
+++ client/src/customers/customers.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './customers.css';
+
+class Customers extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            book: []
+        }
+    }
+
+    componentDidMount() {}
+
+    handleSearch() {
+        const books = fetch(`/search`)
+            .then(res => res.json())
+            .then(books => console.log(books))
+        console.log(books);
+
+    }
+
+    render() {
+
+        const handleSearch = () => this.handleSearch();
+        return (
+            <div>
+                <h1>Simple E-Book crawler</h1>
+                <form action="/search" method="GET">
+                    <input type="text" name="bookName" placeholder="Search your Book!" required/>
+                    <button type="submit" onClick={handleSearch()}>Find</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Customers;
