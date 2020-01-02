fe57f1fcd6cf46f16e93fcf75f633abe3f87a235
SJ <sjisyours@gmail.com>
2020-01-02 13:11:40 +0900

    Done able to react view's submit action to touch backend's puppeteer crawling on milie search result, then spread lists on react view.

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,17 +1,63 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
 export default class App extends React.Component {
-    state = {
-        books: []
+    constructor(props) {
+        super(props);
+
+        this.onSubmit = this
+            .onSubmit
+            .bind(this);
+
+        this.onChange = this
+            .onChange
+            .bind(this);
+
+        this.showMilliBooks = this
+            .showMilliBooks
+            .bind(this);
+
+        this.state = {
+            inputBookName: '',
+            books: []
+        }
+
     }
-   
-	componentDidMount() {
-  
+    onChange(e) {
+        e.preventDefault();
+        this.setState({inputBookName: e.target.value});
+        console.log(this.state);
     }
 
-    handleSearching(){
-        
+    async onSubmit(e) {
+        e.preventDefault();
+        const {inputBookName, books} = this.state;
+        console.log(inputBookName);
+
+        await axios
+            .get(`/search?inputBookName=${inputBookName}`)
+            .then(res => res.data)
+            .then(books => this.setState({books: books}));
+
+        console.log(this.state.books);
+
+    }
+
+    showMilliBooks() {
+        const {books} = this.state;
+        if (books.length > 0) {
+            return (
+                <ul>
+                    {books.map((book, i) => <li key={i}>
+                        <h6>
+                            {book}
+                        </h6>
+                    </li>)
+}
+                </ul>
+            )
+        }
     }
 
     render() {
@@ -21,12 +67,19 @@ export default class App extends React.Component {
                 <h1>Book List</h1>
                 <p>this is the page for crawling the all Korean eBook, from major eBook stores
                     in S.Korea</p>
-                <form action="/search" method="GET">
-                    <input title="Which book are you looking for?" name="bookName"></input>
+                <form onSubmit={this.onSubmit}>
+
+                    <input
+                        placeholder="Search Book Name"
+                        onChange={this.onChange}
+                        name="inputBookName"></input>
+
                     <button type="submit">Search</button>
+
                 </form>
-            
-            <h1>Result</h1>
+
+                <h1>Result</h1>
+                {this.showMilliBooks()}
             </div>
         );
     }
